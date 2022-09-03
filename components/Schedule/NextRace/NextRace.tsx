a4c1f283a029670462d179f58a9c459cb9390364
Paweł Kromołowski <cater981pl@gmail.com>
2022-09-03 16:53:49 +0200

    getting flag for next race

--- components/Schedule/NextRace/NextRace.tsx
+++ components/Schedule/NextRace/NextRace.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import { useEffect } from 'react';
 import { NextRaceType } from '../../../utils/types';
 import styles from './NextRace.module.css';
 
@@ -9,6 +11,12 @@ const NextRace = ({ data }: Props) => {
   return (
     <section className={styles.section}>
       <header className={styles.header}>Next Race: {data.name}</header>
+      <Image
+        src={`https://countryflagsapi.com/png/${data.country.toLowerCase()}`}
+        alt={data.country}
+        width={40}
+        height={30}
+      />
     </section>
   );
 };

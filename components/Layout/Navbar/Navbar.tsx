b1d43b93dd612bb18cea4bdebb96f9492d471583
Paweł Kromołowski <cater981pl@gmail.com>
2022-09-03 13:59:19 +0200

    navbar links

--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
-import Header from './Header';
+
+import Link from 'next/link';
+
 import styles from './Navbar.module.css';
+
+import Header from './Header';
 
 import {
   AiOutlineHome,
@@ -21,91 +25,101 @@ const Navbar = () => {
     <nav className={`${styles.nav} ${navOpen === true && styles.open}`}>
       <div className={styles.container}>
         <Header openNavHandler={openNavHandler} navOpen={navOpen} />
-        <section className={styles.section}>
-          <div
-            className={`${styles.compact} ${
-              navOpen === true && styles.iconOpen
-            }`}
-          >
-            <AiOutlineHome />
-            <p
-              className={`${styles.compactText} ${
-                navOpen === true && styles.openCompact
+        <Link href='/'>
+          <section className={styles.section}>
+            <div
+              className={`${styles.compact} ${
+                navOpen === true && styles.iconOpen
               }`}
             >
-              Home
-            </p>
-          </div>
-          <p className={styles.sectionText}>Home</p>
-        </section>
-        <section className={styles.section}>
-          <div
-            className={`${styles.compact} ${
-              navOpen === true && styles.iconOpen
-            }`}
-          >
-            <AiOutlineCalendar />
-            <p
-              className={`${styles.compactText} ${
-                navOpen === true && styles.openCompact
+              <AiOutlineHome />
+              <p
+                className={`${styles.compactText} ${
+                  navOpen === true && styles.openCompact
+                }`}
+              >
+                Home
+              </p>
+            </div>
+            <p className={styles.sectionText}>Home</p>
+          </section>
+        </Link>
+        <Link href='/schedule'>
+          <section className={styles.section}>
+            <div
+              className={`${styles.compact} ${
+                navOpen === true && styles.iconOpen
               }`}
             >
-              Schedule
-            </p>
-          </div>
-          <p className={styles.sectionText}>Schedule</p>
-        </section>
-        <section className={styles.section}>
-          <div
-            className={`${styles.compact} ${
-              navOpen === true && styles.iconOpen
-            }`}
-          >
-            <AiOutlineTrophy />
-            <p
-              className={`${styles.compactText} ${
-                navOpen === true && styles.openCompact
+              <AiOutlineCalendar />
+              <p
+                className={`${styles.compactText} ${
+                  navOpen === true && styles.openCompact
+                }`}
+              >
+                Schedule
+              </p>
+            </div>
+            <p className={styles.sectionText}>Schedule</p>
+          </section>
+        </Link>
+        <Link href='/results'>
+          <section className={styles.section}>
+            <div
+              className={`${styles.compact} ${
+                navOpen === true && styles.iconOpen
               }`}
             >
-              Results
-            </p>
-          </div>
-          <p className={styles.sectionText}>Results</p>
-        </section>
-        <section className={styles.section}>
-          <div
-            className={`${styles.compact} ${
-              navOpen === true && styles.iconOpen
-            }`}
-          >
-            <AiOutlineTable />
-            <p
-              className={`${styles.compactText} ${
-                navOpen === true && styles.openCompact
+              <AiOutlineTrophy />
+              <p
+                className={`${styles.compactText} ${
+                  navOpen === true && styles.openCompact
+                }`}
+              >
+                Results
+              </p>
+            </div>
+            <p className={styles.sectionText}>Results</p>
+          </section>
+        </Link>
+        <Link href='/standings'>
+          <section className={styles.section}>
+            <div
+              className={`${styles.compact} ${
+                navOpen === true && styles.iconOpen
               }`}
             >
-              Standings
-            </p>
-          </div>
-          <p className={styles.sectionText}>Standings</p>
-        </section>
-        <section className={styles.section}>
-          <div
-            className={`${styles.compact} ${
-              navOpen === true && styles.iconOpen
-            }`}
-          >
-            <AiOutlineHistory />
-            <p
-              className={`${styles.compactText} ${
-                navOpen === true && styles.openCompact
+              <AiOutlineTable />
+              <p
+                className={`${styles.compactText} ${
+                  navOpen === true && styles.openCompact
+                }`}
+              >
+                Standings
+              </p>
+            </div>
+            <p className={styles.sectionText}>Standings</p>
+          </section>
+        </Link>
+        <Link href='/history'>
+          <section className={styles.section}>
+            <div
+              className={`${styles.compact} ${
+                navOpen === true && styles.iconOpen
               }`}
             >
-              History
-            </p>
-          </div>
-          <p className={styles.sectionText}>History</p>
-        </section>
+              <AiOutlineHistory />
+              <p
+                className={`${styles.compactText} ${
+                  navOpen === true && styles.openCompact
+                }`}
+              >
+                History
+              </p>
+            </div>
+            <p className={styles.sectionText}>History</p>
+          </section>
+        </Link>
       </div>
     </nav>
   );

6a6d5af6a1f86b4088e3b63b4afbaa1fe1878cd3
Paweł Kromołowski <cater981pl@gmail.com>
2022-09-15 13:53:32 +0200

    standings loader

--- components/Standings/Constructor.tsx
+++ components/Standings/Constructor.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import ResultLoader from '../Loaders/ResultLoader/ResultLoader';
 import styles from './Constructor.module.css';
 
 type Props = {
@@ -13,36 +14,44 @@ type Constructor = {
 
 const Constructor = ({ data }: Props) => {
   const [constructorImg, setConstructorImg] = useState<Constructor>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchConstructor = async () => {
+      setLoading(true);
       const req = await fetch('/api/constructors');
       const res = await req.json();
 
       if (data.Constructor.constructorId in res) {
         setConstructorImg(res[data.Constructor.constructorId]);
       }
+      setLoading(false);
     };
 
     fetchConstructor().catch((err) => console.error(err));
   }, [data]);
 
   return (
-    <div className={styles.constructorContainer}>
-      <p className={styles.position}>{data.position}</p>
-      <p className={styles.name}>{data.Constructor.name}</p>
-      {constructorImg && (
-        <div className={styles.imageContainer}>
-          <Image
-            src={constructorImg?.image}
-            alt={constructorImg.name}
-            layout='fill'
-            className={styles.image}
-          />
+    <>
+      {loading && <ResultLoader />}
+      {!loading && (
+        <div className={styles.constructorContainer}>
+          <p className={styles.position}>{data.position}</p>
+          <p className={styles.name}>{data.Constructor.name}</p>
+          {constructorImg && (
+            <div className={styles.imageContainer}>
+              <Image
+                src={constructorImg?.image}
+                alt={constructorImg.name}
+                layout='fill'
+                className={styles.image}
+              />
+            </div>
+          )}
+          <p className={styles.points}>{data.points}</p>
         </div>
       )}
-      <p className={styles.points}>{data.points}</p>
-    </div>
+    </>
   );
 };
 
--- components/Standings/Driver.tsx
+++ components/Standings/Driver.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import ResultLoader from '../Loaders/ResultLoader/ResultLoader';
 
 import styles from './Driver.module.css';
 
@@ -14,38 +15,46 @@ type Constructor = {
 
 const Driver = ({ data }: Props) => {
   const [constructorImg, setConstructorImg] = useState<Constructor>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchConstructor = async () => {
+      setLoading(true);
       const req = await fetch('/api/constructors');
       const res = await req.json();
 
       if (data.Constructors[0].constructorId in res) {
         setConstructorImg(res[data.Constructors[0].constructorId]);
       }
+      setLoading(false);
     };
 
     fetchConstructor().catch((err) => console.error(err));
   }, [data]);
 
   return (
-    <div className={styles.driver}>
-      <p className={styles.position}>{data.position}</p>
-      <p className={styles.name}>
-        {data.Driver.givenName} {data.Driver.familyName}
-      </p>
-      {constructorImg && (
-        <div className={styles.imageContainer}>
-          <Image
-            src={constructorImg?.image}
-            alt={constructorImg.name}
-            layout='fill'
-            className={styles.image}
-          />
+    <>
+      {loading && <ResultLoader />}
+      {!loading && (
+        <div className={styles.driver}>
+          <p className={styles.position}>{data.position}</p>
+          <p className={styles.name}>
+            {data.Driver.givenName} {data.Driver.familyName}
+          </p>
+          {constructorImg && (
+            <div className={styles.imageContainer}>
+              <Image
+                src={constructorImg?.image}
+                alt={constructorImg.name}
+                layout='fill'
+                className={styles.image}
+              />
+            </div>
+          )}
+          <p className={styles.points}>{data.points}</p>
         </div>
       )}
-      <p className={styles.points}>{data.points}</p>
-    </div>
+    </>
   );
 };
 
